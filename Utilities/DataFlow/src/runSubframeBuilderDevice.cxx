381a866a30a6e22ebe95c42e1f5c91681801cdac
Matthias Richter <Matthias.Richter@scieq.net>
2017-02-09 18:43:41 +0100

    Fixup: corrected option description
    	modified:   ../src/runSubframeBuilderDevice.cxx

--- Utilities/DataFlow/src/runSubframeBuilderDevice.cxx
+++ Utilities/DataFlow/src/runSubframeBuilderDevice.cxx
@@ -8,7 +8,7 @@ void addCustomOptions(bpo::options_description& options)
   options.add_options()
     (AliceO2::DataFlow::SubframeBuilderDevice::OptionKeyDuration,
      bpo::value<uint32_t>()->default_value(10000),
-     "Name of the input channel")
+     "Time frame duration")
     (AliceO2::DataFlow::SubframeBuilderDevice::OptionKeyInputChannelName,
      bpo::value<std::string>()->default_value("input"),
      "Name of the input channel")
