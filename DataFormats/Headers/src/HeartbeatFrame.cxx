7c01a1342f337b35845db5e80ac74e5ce515a994
Matthias Richter <Matthias.Richter@scieq.net>
2017-02-09 18:44:50 +0100

    Fixup: added missing header properties
    	modified:   ../../../DataFormats/Headers/src/HeartbeatFrame.cxx

--- DataFormats/Headers/src/HeartbeatFrame.cxx
+++ DataFormats/Headers/src/HeartbeatFrame.cxx
@@ -7,3 +7,7 @@
 
 // define the description with a terminating '0' (meaning 15 characters)
 const AliceO2::Header::DataDescription AliceO2::Header::gDataDescriptionHeartbeatFrame("HEARTBEATFRAME");
+
+const uint32_t AliceO2::Header::HeartbeatFrameEnvelope::sVersion = 1;
+const AliceO2::Header::HeaderType AliceO2::Header::HeartbeatFrameEnvelope::sHeaderType= String2<uint64_t>("HBFEnvel");
+const AliceO2::Header::SerializationMethod AliceO2::Header::HeartbeatFrameEnvelope::sSerializationMethod = AliceO2::Header::gSerializationMethodNone;
