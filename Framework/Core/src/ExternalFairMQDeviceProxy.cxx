d4b7581ff4df4503b08381f7fcbf039b1d2e2609
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2022-02-16 13:55:55 +0100

    DPL: fix start-stop-start transition

--- Framework/Core/src/ExternalFairMQDeviceProxy.cxx
+++ Framework/Core/src/ExternalFairMQDeviceProxy.cxx
@@ -423,7 +423,7 @@ DataProcessorSpec specifyExternalFairMQDeviceProxy(char const* name,
         for (auto const& channel : outputChannels) {
           DataProcessingHelpers::sendEndOfStream(*device, channel);
         }
-        control->readyToQuit(QuitRequest::Me);
+      //  control->readyToQuit(QuitRequest::Me);
       }
     };
 
