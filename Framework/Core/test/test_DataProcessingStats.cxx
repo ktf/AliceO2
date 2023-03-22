beb39fbc7939a72dcf76f0e199ad506b255049b0
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-03-22 21:32:32 +0100

    DPL: improve metric sending

--- Framework/Core/test/test_DataProcessingStats.cxx
+++ Framework/Core/test/test_DataProcessingStats.cxx
@@ -19,7 +19,8 @@ using namespace o2::framework;
 enum TestMetricsId {
   DummyMetric = 0,
   DummyMetric2 = 1,
-  Missing = 2
+  Missing = 2,
+  ZeroSize = 3,
 };
 
 using namespace o2::framework;
@@ -34,8 +35,11 @@ TEST_CASE("DataProcessingStats")
   REQUIRE_THROWS(stats.registerMetric({"dummy_metric", DummyMetric2}));
   /// Registering with a different name should throw.
   REQUIRE_THROWS(stats.registerMetric({"dummy_metric2", DummyMetric}));
+  /// Registering with a different name should throw.
+  REQUIRE_THROWS(stats.registerMetric({"", ZeroSize}));
 
   stats.registerMetric({"dummy_metric2", DummyMetric2});
+  REQUIRE(stats.metricsNames[DummyMetric] == "dummy_metric");
   stats.updateStats({DummyMetric, DataProcessingStats::Op::Add, 1});
   REQUIRE_THROWS(stats.updateStats({Missing, DataProcessingStats::Op::Add, 1}));
   REQUIRE(stats.nextCmd.load() == 1);
@@ -69,7 +73,7 @@ TEST_CASE("DataProcessingStats")
   REQUIRE(stats.updated[DummyMetric2] == false);
 
   std::vector<std::string> updated;
-  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value) {
+  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value, DataProcessingStats::Kind kind) {
     updated.emplace_back(name);
   };
 
@@ -313,7 +317,7 @@ TEST_CASE("DataProcessingStatsPublishing")
   REQUIRE(stats.metrics[DummyMetric] == 0);
 
   std::vector<std::string> updated;
-  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value) {
+  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value, DataProcessingStats::Kind) {
     updated.emplace_back(name);
   };
 
@@ -357,7 +361,7 @@ TEST_CASE("DataProcessingStatsPublishingRepeated")
   REQUIRE(stats.metrics[DummyMetric] == 0);
 
   std::vector<std::string> updated;
-  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value) {
+  auto simpleFlush = [&updated](std::string const& name, int64_t timestamp, int64_t value, DataProcessingStats::Kind) {
     updated.emplace_back(name);
   };
 
