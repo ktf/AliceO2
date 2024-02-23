443c2d7b12d0ca2965fa6e04461f943724a57b1e
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-02-23 08:14:05 +0100

    DPL: drop support for serialising / deserializing schema

--- Framework/Core/include/Framework/TMessageSerializer.h
+++ Framework/Core/include/Framework/TMessageSerializer.h
@@ -26,9 +26,7 @@
 #include <mutex>
 #include <cstddef>
 
-namespace o2
-{
-namespace framework
+namespace o2::framework
 {
 class FairTMessage;
 
@@ -51,28 +49,23 @@ class FairTMessage : public TMessage
 struct TMessageSerializer {
   using StreamerList = std::vector<TVirtualStreamerInfo*>;
   using CompressionLevel = int;
-  enum class CacheStreamers { yes,
-                              no };
 
   static void Serialize(fair::mq::Message& msg, const TObject* input,
-                        CacheStreamers streamers = CacheStreamers::no,
                         CompressionLevel compressionLevel = -1);
 
   template <typename T>
   static void Serialize(fair::mq::Message& msg, const T* input, const TClass* cl, //
-                        CacheStreamers streamers = CacheStreamers::no,            //
                         CompressionLevel compressionLevel = -1);
 
   template <typename T = TObject>
   static void Deserialize(const fair::mq::Message& msg, std::unique_ptr<T>& output);
 
   static void serialize(FairTMessage& msg, const TObject* input,
-                        CacheStreamers streamers = CacheStreamers::no,
                         CompressionLevel compressionLevel = -1);
 
   template <typename T>
   static void serialize(FairTMessage& msg, const T* input,                               //
-                        const TClass* cl, CacheStreamers streamers = CacheStreamers::no, //
+                        const TClass* cl, 
                         CompressionLevel compressionLevel = -1);
 
   template <typename T = TObject>
@@ -80,45 +73,22 @@ struct TMessageSerializer {
   template <typename T = TObject>
   static inline std::unique_ptr<T> deserialize(std::byte* buffer, size_t size);
 
-  // load the schema information from a message/buffer
-  static void loadSchema(const fair::mq::Message& msg);
-  static void loadSchema(gsl::span<std::byte> buffer);
-
-  // write the schema into an empty message/buffer
-  static void fillSchema(fair::mq::Message& msg, const StreamerList& streamers);
-  static void fillSchema(FairTMessage& msg, const StreamerList& streamers);
-
   // get the streamers
   static StreamerList getStreamers();
 
-  // update the streamer list with infos appropriate for this type
-  static void updateStreamers(const TObject* object);
-
  private:
-  // update the cache of streamer infos for serialized classes
-  static void updateStreamers(const FairTMessage& message, StreamerList& streamers);
-
-  // for now this is a static, maybe it would be better to move the storage somewhere else?
-  static StreamerList sStreamers;
-  static std::mutex sStreamersLock;
 };
 
 inline void TMessageSerializer::serialize(FairTMessage& tm, const TObject* input,
-                                          CacheStreamers streamers,
                                           CompressionLevel compressionLevel)
 {
-  return serialize(tm, input, nullptr, streamers, compressionLevel);
+  return serialize(tm, input, nullptr, compressionLevel);
 }
 
 template <typename T>
 inline void TMessageSerializer::serialize(FairTMessage& tm, const T* input,           //
-                                          const TClass* cl, CacheStreamers streamers, //
-                                          CompressionLevel compressionLevel)
+                                          const TClass* cl, CompressionLevel compressionLevel)
 {
-  if (streamers == CacheStreamers::yes) {
-    tm.EnableSchemaEvolution(true);
-  }
-
   if (compressionLevel >= 0) {
     // if negative, skip to use ROOT default
     tm.SetCompressionLevel(compressionLevel);
@@ -129,10 +99,6 @@ inline void TMessageSerializer::serialize(FairTMessage& tm, const T* input,     
     tm.WriteObject(input);
   } else {
     tm.WriteObjectAny(input, cl);
-  }
-
-  if (streamers == CacheStreamers::yes) {
-    updateStreamers(tm, sStreamers);
   }
 }
 
@@ -172,12 +138,11 @@ inline void FairTMessage::free(void* /*data*/, void* hint)
 }
 
 inline void TMessageSerializer::Serialize(fair::mq::Message& msg, const TObject* input,
-                                          TMessageSerializer::CacheStreamers streamers,
                                           TMessageSerializer::CompressionLevel compressionLevel)
 {
   std::unique_ptr<FairTMessage> tm = std::make_unique<FairTMessage>(kMESS_OBJECT);
 
-  serialize(*tm, input, input->Class(), streamers, compressionLevel);
+  serialize(*tm, input, input->Class(), compressionLevel);
 
   msg.Rebuild(tm->Buffer(), tm->BufferSize(), FairTMessage::free, tm.get());
   tm.release();
@@ -186,12 +151,11 @@ inline void TMessageSerializer::Serialize(fair::mq::Message& msg, const TObject*
 template <typename T>
 inline void TMessageSerializer::Serialize(fair::mq::Message& msg, const T* input,       //
                                           const TClass* cl,                             //
-                                          TMessageSerializer::CacheStreamers streamers, //
                                           TMessageSerializer::CompressionLevel compressionLevel)
 {
   std::unique_ptr<FairTMessage> tm = std::make_unique<FairTMessage>(kMESS_OBJECT);
 
-  serialize(*tm, input, cl, streamers, compressionLevel);
+  serialize(*tm, input, cl, compressionLevel);
 
   msg.Rebuild(tm->Buffer(), tm->BufferSize(), FairTMessage::free, tm.get());
   tm.release();
@@ -203,12 +167,6 @@ inline void TMessageSerializer::Deserialize(const fair::mq::Message& msg, std::u
   // we know the message will not be modified by this,
   // so const_cast should be OK here(IMHO).
   output = deserialize(as_span(msg));
-}
-
-inline TMessageSerializer::StreamerList TMessageSerializer::getStreamers()
-{
-  std::lock_guard<std::mutex> lock{TMessageSerializer::sStreamersLock};
-  return sStreamers;
 }
 
 // gsl::narrow is used to do a runtime narrowing check, this might be a bit paranoid,
@@ -224,6 +182,5 @@ inline gsl::span<std::byte> as_span(const FairTMessage& msg)
                              gsl::narrow<gsl::span<std::byte>::size_type>(msg.BufferSize())};
 }
 
-} // namespace framework
 } // namespace o2
 #endif // FRAMEWORK_TMESSAGESERIALIZER_H
