96f9f2ab392578b32556a70c9f6e09ee75933f6b
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-05-13 09:39:09 +0200

    DPL Analysis: provide metadata to workflow construction

--- Framework/AnalysisSupport/src/Plugin.cxx
+++ Framework/AnalysisSupport/src/Plugin.cxx
@@ -12,7 +12,17 @@
 #include "Framework/AlgorithmSpec.h"
 #include "Framework/ServiceSpec.h"
 #include "Framework/ServiceMetricsInfo.h"
+#include "Framework/ConfigParamDiscovery.h"
+#include "Framework/Capability.h"
+#include "Framework/Signpost.h"
 #include "AODJAlienReaderHelpers.h"
+#include <TFile.h>
+#include <TMap.h>
+#include <TObjString.h>
+#include <TString.h>
+#include <fmt/format.h>
+
+O2_DECLARE_DYNAMIC_LOG(analysis_support);
 
 struct ROOTFileReader : o2::framework::AlgorithmPlugin {
   o2::framework::AlgorithmSpec create() override
@@ -54,7 +64,139 @@ struct RunSummary : o2::framework::ServicePlugin {
   }
 };
 
+auto lookForAodFile = [](ConfigParamRegistry& registry, int argc, char** argv) -> bool {
+  if (registry.hasOption("aod-file") && registry.isSet("aod-file")) {
+    for (size_t i = 0; i < argc; i++) {
+      std::string_view arg = argv[i];
+      if (arg.starts_with("--aod-metadata-")) {
+        return false;
+      }
+    }
+    LOGP(info, "Will read metadata from AOD file.");
+    return true;
+  }
+  return false;
+};
+
+auto lookForCommandLineOptions = [](ConfigParamRegistry& registry, int argc, char** argv) -> bool {
+  for (size_t i = 0; i < argc; i++) {
+    std::string_view arg = argv[i];
+    if (arg.starts_with("--aod-metadata-")) {
+      LOGP(info, "Metadata found in arguments. Populating from them.");
+      return true;
+    }
+  }
+  return false;
+};
+
+struct DiscoverMetadataInAODCapability : o2::framework::CapabilityPlugin {
+  Capability* create() override
+  {
+    return new Capability{
+      .name = "DiscoverMetadataInAODCapability",
+      .checkIfNeeded = lookForAodFile,
+      .requiredPlugin = "O2FrameworkAnalysisSupport:DiscoverMetadataInAOD"};
+  }
+};
+
+// Trigger discovery of metadata from command line, if needed.
+struct DiscoverMetadataInCommandLineCapability : o2::framework::CapabilityPlugin {
+  Capability* create() override
+  {
+    return new Capability{
+      .name = "DiscoverMetadataInCommandLineCapability",
+      .checkIfNeeded = lookForCommandLineOptions,
+      .requiredPlugin = "O2FrameworkAnalysisSupport:DiscoverMetadataInCommandLine"};
+  }
+};
+
+struct DiscoverMetadataInAOD : o2::framework::ConfigDiscoveryPlugin {
+  ConfigDiscovery* create() override
+  {
+    return new ConfigDiscovery{
+      .init = []() {},
+      .discover = [](ConfigParamRegistry& registry, int argc, char** argv) -> std::vector<ConfigParamSpec> {
+        auto filename = registry.get<std::string>("aod-file");
+        if (filename.empty()) {
+          return {};
+        }
+        LOGP(info, "Loading metadata from file {} in PID {}", filename, getpid());
+        std::vector<ConfigParamSpec> results;
+        TFile* currentFile = nullptr;
+        if (filename.at(0) == '@') {
+          filename.erase(0, 1);
+          // read the text file and set filename to the contents of the first line
+          std::ifstream file(filename);
+          if (!file.is_open()) {
+            LOGP(fatal, "Couldn't open file \"{}\"!", filename);
+          }
+          std::getline(file, filename);
+          file.close();
+          currentFile = TFile::Open(filename.c_str());
+        } else {
+          currentFile = TFile::Open(filename.c_str());
+        }
+        if (!currentFile) {
+          LOGP(fatal, "Couldn't open file \"{}\"!", filename);
+        }
+
+        // Get the metadata, if any
+        auto m = (TMap*)currentFile->Get("metaData");
+        if (!m) {
+          LOGP(warning, "No metadata found in file \"{}\"", filename);
+          return results;
+        }
+        auto it = m->MakeIterator();
+
+        // Serialise metadata into a ; separated string with : separating key and value
+        bool first = true;
+        while (auto obj = it->Next()) {
+          if (first) {
+            LOGP(info, "Metadata for file \"{}\":", filename);
+            first = false;
+          }
+          auto objString = (TObjString*)m->GetValue(obj);
+          LOGP(info, "- {}: {}", obj->GetName(), objString->String().Data());
+          std::string key = "aod-metadata-" + std::string(obj->GetName());
+          char const* value = strdup(objString->String());
+          results.push_back(ConfigParamSpec{key, VariantType::String, value, {"Metadata in AOD"}});
+        }
+        return results;
+      }};
+  }
+};
+
+struct DiscoverMetadataInCommandLine : o2::framework::ConfigDiscoveryPlugin {
+  ConfigDiscovery* create() override
+  {
+    return new ConfigDiscovery{
+      .init = []() {},
+      .discover = [](ConfigParamRegistry& registry, int argc, char** argv) -> std::vector<ConfigParamSpec> {
+        O2_SIGNPOST_ID_GENERATE(sid, analysis_support);
+        O2_SIGNPOST_EVENT_EMIT(analysis_support, sid, "DiscoverMetadataInCommandLine",
+                               "Discovering metadata for analysis from well known environment variables.");
+        std::vector<ConfigParamSpec> results;
+        for (size_t i = 0; i < argc; i++) {
+          std::string_view arg = argv[i];
+          if (!arg.starts_with("--aod-metadata")) {
+            continue;
+          }
+          std::string key = arg.data() + 2;
+          std::string value = argv[i + 1];
+          O2_SIGNPOST_EVENT_EMIT(analysis_support, sid, "DiscoverMetadataInCommandLine",
+                                 "Found %{public}s with value %{public}s.", key.c_str(), value.c_str());
+          results.push_back(ConfigParamSpec{key, VariantType::String, value, {"Metadata in command line"}});
+        }
+        return results;
+      }};
+  }
+};
+
 DEFINE_DPL_PLUGINS_BEGIN
 DEFINE_DPL_PLUGIN_INSTANCE(ROOTFileReader, CustomAlgorithm);
 DEFINE_DPL_PLUGIN_INSTANCE(RunSummary, CustomService);
+DEFINE_DPL_PLUGIN_INSTANCE(DiscoverMetadataInAODCapability, Capability);
+DEFINE_DPL_PLUGIN_INSTANCE(DiscoverMetadataInAOD, ConfigDiscovery);
+DEFINE_DPL_PLUGIN_INSTANCE(DiscoverMetadataInCommandLineCapability, Capability);
+DEFINE_DPL_PLUGIN_INSTANCE(DiscoverMetadataInCommandLine, ConfigDiscovery);
 DEFINE_DPL_PLUGINS_END
