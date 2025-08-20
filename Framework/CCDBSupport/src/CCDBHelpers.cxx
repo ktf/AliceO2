2a1015d2cf21eb9e3f760d441ad9b04445c80a4b
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-08-20 15:01:57 +0200

    DPL CCDB: adapt to newly introduced helper.
    The new helper is a bit more generic and can be used for also for the case
    where we multiplex multiple CCDB object on top of the same output, like
    we do in analysis.

--- Framework/CCDBSupport/src/CCDBHelpers.cxx
+++ Framework/CCDBSupport/src/CCDBHelpers.cxx
@@ -23,121 +23,19 @@
 #include "Framework/Signpost.h"
 #include <TError.h>
 #include <TMemFile.h>
+#include <ctime>
 
 O2_DECLARE_DYNAMIC_LOG(ccdb);
 
 namespace o2::framework
 {
 
-bool isPrefix(std::string_view prefix, std::string_view full)
-{
-  return prefix == full.substr(0, prefix.size());
-}
-
-CCDBHelpers::ParserResult CCDBHelpers::parseRemappings(char const* str)
-{
-  std::unordered_map<std::string, std::string> remappings;
-  std::string currentUrl = "";
-
-  enum ParsingStates {
-    IN_BEGIN,
-    IN_BEGIN_URL,
-    IN_BEGIN_TARGET,
-    IN_END_TARGET,
-    IN_END_URL
-  };
-  ParsingStates state = IN_BEGIN;
-
-  while (true) {
-    switch (state) {
-      case IN_BEGIN: {
-        if (*str == 0) {
-          return {remappings, ""};
-        }
-        state = IN_BEGIN_URL;
-      }
-      case IN_BEGIN_URL: {
-        if ((strncmp("http://", str, 7) != 0) && (strncmp("https://", str, 8) != 0 && (strncmp("file://", str, 7) != 0))) {
-          return {remappings, "URL should start with either http:// or https:// or file://"};
-        }
-        state = IN_END_URL;
-      } break;
-      case IN_END_URL: {
-        char const* c = strchr(str, '=');
-        if (c == nullptr) {
-          return {remappings, "Expecting at least one target path, missing `='?"};
-        }
-        if ((c - str) == 0) {
-          return {remappings, "Empty url"};
-        }
-        currentUrl = std::string_view(str, c - str);
-        state = IN_BEGIN_TARGET;
-        str = c + 1;
-      } break;
-      case IN_BEGIN_TARGET: {
-        if (*str == 0) {
-          return {remappings, "Empty target"};
-        }
-        state = IN_END_TARGET;
-      } break;
-      case IN_END_TARGET: {
-        char const* c = strpbrk(str, ",;");
-        if (c == nullptr) {
-          if (remappings.count(str)) {
-            return {remappings, fmt::format("Path {} requested more than once.", str)};
-          }
-          remappings[std::string(str)] = currentUrl;
-          return {remappings, ""};
-        }
-        if ((c - str) == 0) {
-          return {remappings, "Empty target"};
-        }
-        auto key = std::string(str, c - str);
-        if (remappings.count(str)) {
-          return {remappings, fmt::format("Path {} requested more than once.", key)};
-        }
-        remappings[key] = currentUrl;
-        if (*c == ';') {
-          state = IN_BEGIN_URL;
-        } else {
-          state = IN_BEGIN_TARGET;
-        }
-        str = c + 1;
-      } break;
-    }
-  }
-}
-
-void initialiseHelper(CCDBFetcherHelper& helper, ConfigParamRegistry const& options, std::vector<o2::framework::OutputRoute> const& outputRoutes)
-{
+// Fill valid routes. Here we consider only the routes which have 
+// Lifetime::Condition. Notice the way we do it for analysis will be
+// different.
+namespace {
+void fillValidRoutes(CCDBFetcherHelper& helper, std::vector<o2::framework::OutputRoute> const& outputRoutes) {
   std::unordered_map<std::string, bool> accountedSpecs;
-  auto defHost = options.get<std::string>("condition-backend");
-  auto checkRate = options.get<int>("condition-tf-per-query");
-  auto checkMult = options.get<int>("condition-tf-per-query-multiplier");
-  helper.timeToleranceMS = options.get<int64_t>("condition-time-tolerance");
-  helper.queryPeriodGlo = checkRate > 0 ? checkRate : std::numeric_limits<int>::max();
-  helper.queryPeriodFactor = checkMult > 0 ? checkMult : 1;
-  LOGP(info, "CCDB Backend at: {}, validity check for every {} TF{}", defHost, helper.queryPeriodGlo, helper.queryPeriodFactor == 1 ? std::string{} : fmt::format(", (query for high-rate objects downscaled by {})", helper.queryPeriodFactor));
-  LOGP(info, "Hook to enable signposts for CCDB messages at {}", (void*)&private_o2_log_ccdb->stacktrace);
-  auto remapString = options.get<std::string>("condition-remap");
-  CCDBHelpers::ParserResult result = CCDBHelpers::parseRemappings(remapString.c_str());
-  if (!result.error.empty()) {
-    throw runtime_error_f("Error while parsing remapping string %s", result.error.c_str());
-  }
-  helper.remappings = result.remappings;
-  helper.apis[""].init(defHost); // default backend
-  LOGP(info, "Initialised default CCDB host {}", defHost);
-  //
-  for (auto& entry : helper.remappings) { // init api instances for every host seen in the remapping
-    if (helper.apis.find(entry.second) == helper.apis.end()) {
-      helper.apis[entry.second].init(entry.second);
-      LOGP(info, "Initialised custom CCDB host {}", entry.second);
-    }
-    LOGP(info, "{} is remapped to {}", entry.first, entry.second);
-  }
-  helper.createdNotBefore = std::to_string(options.get<int64_t>("condition-not-before"));
-  helper.createdNotAfter = std::to_string(options.get<int64_t>("condition-not-after"));
-
   for (auto& route : outputRoutes) {
     if (route.matcher.lifetime != Lifetime::Condition) {
       continue;
@@ -155,6 +53,7 @@ void initialiseHelper(CCDBFetcherHelper& helper, ConfigParamRegistry const& opti
       }
     }
   }
+}
 }
 
 auto getOrbitResetTime(o2::pmr::vector<char> const& v) -> Long64_t
@@ -180,7 +79,8 @@ AlgorithmSpec CCDBHelpers::fetchFromCCDB()
 {
   return adaptStateful([](CallbackService& callbacks, ConfigParamRegistry const& options, DeviceSpec const& spec) {
       std::shared_ptr<CCDBFetcherHelper> helper = std::make_shared<CCDBFetcherHelper>();
-      initialiseHelper(*helper, options, spec.outputs);
+      CCDBFetcherHelper::initialiseHelper(*helper, options);
+      fillValidRoutes(*helper, spec.outputs);
       /// Add a callback on stop which dumps the statistics for the caching per
       /// path
       callbacks.set<CallbackService::Id::Stop>([helper]() {
@@ -279,6 +179,33 @@ AlgorithmSpec CCDBHelpers::fetchFromCCDB()
         // Fetch the rest of the objects.
         O2_SIGNPOST_EVENT_EMIT(ccdb, sid, "fetchFromCCDB", "Fetching objects. Run %{public}s. OrbitResetTime %lld. Creation %lld. Timestamp %lld. firstTForbit %" PRIu32,
             dtc.runNumber.data(), orbitResetTime, timingInfo.creation, timestamp, timingInfo.firstTForbit);
+        std::vector<CCDBFetcherHelper::FetchOp> ops;
+        int runNumber = 0;
+        std::string ccdbMetadataPrefix = "ccdb-metadata-";
+        for (auto &route : helper->routes) {
+          CCDBFetcherHelper::FetchOp op{.spec = route.matcher, .timestamp = timestamp, .runNumber = std::stoi(dtc.runNumber)};
+          for (auto& meta : route.matcher.metadata) {
+            if (meta.name == "ccdb-path") {
+              op.url = meta.defaultValue.get<std::string>();
+            } else if (meta.name == "ccdb-run-dependent" && meta.defaultValue.get<int>() > 0) {
+              if (meta.defaultValue.get<int>() == 1) {
+                op.runNumber = runNumber;
+              } else if (meta.defaultValue.get<int>() == 2) {
+                op.timestamp = runNumber;
+              } else {
+                LOGP(fatal, "Undefined ccdb-run-dependent option {} for spec {}", meta.defaultValue.get<int>(), DataSpecUtils::describe(route.matcher));
+              }
+            } else if (meta.name.starts_with(ccdbMetadataPrefix)) {
+              std::string key = meta.name.substr(ccdbMetadataPrefix.size());
+              auto value = meta.defaultValue.get<std::string>();
+              O2_SIGNPOST_EVENT_EMIT(ccdb, sid, "populateCacheWith", "Adding metadata %{public}s: %{public}s to the request", key.data(), value.data());
+              op.metadata.push_back({key, value});
+            } else if (meta.name == "ccdb-query-rate") {
+              op.queryRate = meta.defaultValue.get<int>() * helper->queryPeriodFactor;
+            }
+          }
+          ops.push_back(op);
+        }
 
         CCDBFetcherHelper::populateCacheWith(helper, ops, timingInfo, dtc, allocator);
         O2_SIGNPOST_END(ccdb, _o2_signpost_id_t{(int64_t)timingInfo.timeslice}, "fetchFromCCDB", "Fetching CCDB objects");
